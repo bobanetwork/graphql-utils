ac7d38666a6066f0bf5e4b594c89ff86e5ca128f
cby3149 <cby3149@gmail.com>
2024-08-20 12:21:53 -0700

    Update boba bnb testnet endpoint

--- src/graphql.service.ts
+++ src/graphql.service.ts
@@ -98,7 +98,7 @@ export class GraphQLService {
                 local: 'http://127.0.0.1:8002/subgraphs/name/boba/Bridges',
             },
             [EGraphQLService.AnchorageBridge]: {
-                gql: 'https://graphql.bnb.boba.network/subgraphs/name/anchorage-bridging-boba-bnb',
+                gql: 'https://graphql.testnet.bnb.boba.network/subgraphs/name/anchorage-bridging-boba-bnb-testnet/',
             },
         },
         // Arbitrum Sepolia
